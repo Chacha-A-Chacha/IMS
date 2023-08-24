6ce97b81e2929d6c9beb001ca95376183b6a85e9
Chacha Chacha <calex2607@gmail.com>
2023-08-24 21:14:19 +0000

    templating

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -12,7 +12,7 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username = form.username.data, email = form.email.data)
-        user.set_password(form.password.data)
+        user.set_password(form.password.data) # Set hashed password
         db.session.add(user) # add the new user to our database session (in memory only for now)
         db.session.commit()  # commit all changes in this transaction so they are saved into the DB!
 
@@ -28,19 +28,23 @@ def login():
     if current_user.is_authenticated:
         return redirect(url_for('main.index'))
     form = LoginForm()
+
     if form.validate_on_submit():
         user = User.query.filter_by(username = form.username.data).first()
+
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('auth.login'))
+
         login_user(user, remember = form.remember_me.data)
         return redirect(url_for('main.index'))
+
     return render_template('auth/login.html', title = "Sign In", form = form)
 
 @bp.route('/logout')
 def logout():
     # remove the token from the cookie and log out the user
     # we do it by setting the expiration date to sometime in the past
-    #    resp= make_response(redirect(url_for("main.index")))
+    # resp= make_response(redirect(url_for("main.index")))
     logout_user()
     return redirect(url_for('main.index'))
--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
-from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+
+from app import db
 
 
 class User(db.Model, UserMixin):
@@ -12,6 +14,11 @@ class User(db.Model, UserMixin):
 
 
     # You can add additional fields like 'team_id', 'role', etc.
-
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
     def __repr__(self):
         return '<User {}>'.format(self.username)
