dcc7b441c28716abb3142f3c91f06d34a9efda40
Chacha-A-Chacha <calex2607@gmail.com>
2023-09-07 12:48:27 +0300

    templates

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,15 @@ migrate = Migrate(app, db)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+
+from .main import main as main_blueprint
+app.register_blueprint(main_blueprint)
+
+
+from .auth import auth_bp as auth_blueprint
+app.register_blueprint(auth_blueprint, url_prefix='/auth')
