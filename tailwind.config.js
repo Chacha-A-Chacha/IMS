f542890f72dfdf1393a94cb6da7376436b463892
Chacha-A-Chacha <calex2607@gmail.com>
2023-12-11 12:47:43 +0300

    tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  purge: [],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
-  variants: {},
-  plugins: [
-    // require('@tailwindcss/ui'),
+  content: [
+    "./app/templates/**/*.html",
+    "./app/static/src/**/*.js"
   ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
-
-
-// module.exports = {
-//   content: [
-//     './app/templates/**/*.{html, js}',  // Path to your Flask templates
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
 
